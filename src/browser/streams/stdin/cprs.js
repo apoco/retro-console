d595ecfd57614a3d438cdc4c24638f4ebd2ae4b2
Jacob Page <jacob.h.page@gmail.com>
2016-02-07 11:49:42 -0800

    Fix CPR responses

--- src/browser/streams/stdin/cprs.js
+++ src/browser/streams/stdin/cprs.js
@@ -4,4 +4,4 @@ import cprRequests from '../stdout/cprRequests';
 import charGrid from '../display/state';
 
 export default combine([cprRequests], [charGrid])
-  .map((req, { pos: { row, col } }) => `\u001b[${row};${col}R`);
+  .map(([req, { pos: { row, col } }]) => `\u001b[${row};${col}R`);
